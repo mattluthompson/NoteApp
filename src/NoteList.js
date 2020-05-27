5769a34a3413915d942ba1723c7044b0a4e423bf
annie-sharkey <sharkeyconwaya@gmail.com>
2020-05-27 12:00:23 -0400

    NoteList comments
    Let me know if there's anything unclear conceptually. The comment looks really long but it's just some minor changes I would like to see to a well executed project.

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import NotesCard from './NotesCard';
 
+/*
+Remove this.props.list from state. You can access this in the return by saying
+this.props.list.map ...
 
+A component's state represents the data that its functions will update and control
+in the same file / component. Props is data that we pass around that the child will not
+manipulate. Therefore, in this example, NoteList does not need to have any state because
+we are simply receiving data, not manipulating its contents. (If you had to manipulate the data
+you could use a callback function although this is not preferred.) You can remove the constructor props
+to the end of the this.state statement.
+
+The component as written is a class component that is designed to hold and control state.
+Because NoteList does not need to control its own state, we can write this as a functional component.
+Therefore you can write:
+
+const NoteList = (props) => {
+    return ();
+   }
+   
+Note how in a class component you could just access props by saying this.props.XXX. 
+For our functional component, we pass props in as an argument and then access props
+in our return by saying this.props.XXX.
+*/
 class NoteList extends React.Component {
     constructor(props) {
         super(props);
